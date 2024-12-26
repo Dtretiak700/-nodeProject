412d7d40282d9fc1d293bba9d7cd90fdd53f7d82
Dtretiak700 <d.tretiak700@gmail.com>
2024-12-26 19:42:45 +0100

    first commit

---
+++ backend/MealRoutes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const { getMeal, saveMeals } = require('./MealController');
+const router = Router();
+
+router.get('/', getMeal);
+router.post('/', saveMeals);
+
+module.exports = router;
